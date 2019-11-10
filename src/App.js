a6167d08ed851a13d6fec35048df76a3520fc411
Noi <noi.patsin@gmail.com>
2019-11-09 20:58:58 -0800

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  // async componentDidMount() {
+  //   // console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  //   // change state after loading
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   // reset the state after axios makes request and get response
+  //   // then set users to the data from API. Then set loading back to false
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github users
+  searchUsers = async text => {
+    // console.log(text);
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar title="Github Finder" icon="fa fa-pie-chart" />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
+          <Users loading={this.state.loeading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Navbar.css';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <div className="nav">
+      <h1 className="container">
+        <i className={icon} aria-hidden="true"></i> {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fa fa-pie-chart'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
